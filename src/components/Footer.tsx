c76bceac1a95fb5fdc0edf10316a45d52e737183
HaileabT <haileabtesfaye11@gmail.com>
2025-01-27 06:46:31 -0500

    finished working on header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,43 @@
+import { useContext } from "react";
+import { UserLoggedInContext } from "../App";
+
 export function Footer() {
-  return <></>;
+  const { userLoggedIn } = useContext(UserLoggedInContext);
+  const links = [
+    {
+      text: "Home",
+      href: "/",
+    },
+    {
+      text: "Blogs",
+      href: "/blog",
+    },
+    {
+      text: "Register",
+      href: "/signup",
+    },
+    {
+      text: "Login",
+      href: "/login",
+    },
+  ];
+  return (
+    <>
+      <footer className="flex flex-col justify-center w-full items-center gap-5 bg-secodary-bg py-30 px-10 rounded-xl">
+        <div className="text-alt-bg font-extrabold font-bebas text-4xl w-1/4 text-center">SiMPLE BLOGGING APP</div>
+
+        <ul className="flex flex-col items-center justify-center w-3/4 list-none">
+          {links.map((link) => {
+            if (link.text === "Register" && userLoggedIn) return;
+            if (link.text === "Login" && userLoggedIn) return;
+            return (
+              <li key={link.href} className="hover:text-active-text text-2xl font-light text-alt2-bg ">
+                <a href={link.href}>{link.text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </footer>
+    </>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,88 @@
+import { useContext } from "react";
+import { UserLoggedInContext } from "../App";
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+interface LogoutResponseData {
+  status: string;
+  data: {
+    loggedout: boolean;
+  };
+}
+
 export function Header() {
+  const { userLoggedIn, setUserLoggedIn } = useContext(UserLoggedInContext);
+  setUserLoggedIn(sessionStorage.getItem("logged-in") === "true" || false);
+  const links = [
+    {
+      text: "Home",
+      href: "/",
+    },
+    {
+      text: "Blogs",
+      href: "/blog",
+    },
+  ];
+  const buttons = [
+    {
+      text: "Login",
+      href: "/login",
+    },
+    {
+      text: "Register",
+      href: "/signup",
+    },
+  ];
+
+  const onLogOut = async () => {
+    try {
+      const res = await axios.get<any, AxiosResponse<LogoutResponseData>>("http://localhost:4000/api/v1/auth/logout", {
+        withCredentials: true,
+      });
+      console.log(res.data.data);
+      sessionStorage.removeItem("logged-in");
+      setUserLoggedIn(sessionStorage.getItem("logged-in") === "true" || false);
+    } catch (error: AxiosError | any) {
+      console.log(error?.response.data.error);
+    }
+  };
   return (
     <>
       <header className="flex w-full items-center gap-10">
         <div className="font-extrabold text-2xl w-1/4">SiMPLE BLOGGING APP</div>
 
-        <ul className="flex flex-col items-end w-3/4 list-none">
-          <li className="hover:text-active-text text-lg">
-            <a href="">Home</a>
-          </li>
-          <li className="hover:text-active-text text-lg">
-            <a href="">Blogs</a>
-          </li>
-          <li className="hover:text-active-text text-lg">
-            <a href="">Home</a>
-          </li>
+        <ul className={`flex flex-col items-center w-1/2 list-none`}>
+          {links.map((link) => {
+            return (
+              <li key={link.href} className="hover:text-active-text text-lg font-light">
+                <a href={link.href}>{link.text}</a>
+              </li>
+            );
+          })}
         </ul>
+        {!userLoggedIn && (
+          <ul className="flex flex-wrap justify-end w-1/4 items-end list-none gap-2">
+            {buttons.map((button) => {
+              return (
+                <button
+                  key={button.href}
+                  className="grow shrink basis-[50px] max-w-[100px] bg-secodary-bg rounded-md p-2 text-alt-bg hover:bg-active-text text-lg font-light"
+                >
+                  <a href={button.href}>{button.text}</a>
+                </button>
+              );
+            })}
+          </ul>
+        )}
+        {userLoggedIn && (
+          <div className="flex flex-wrap justify-end w-1/4 items-end list-none gap-2">
+            <button
+              onClick={onLogOut}
+              className=" max-w-[100px] bg-secodary-bg rounded-md p-2 text-alt-bg hover:bg-active-text text-lg font-light"
+            >
+              Log Out
+            </button>
+          </div>
+        )}
       </header>
     </>
   );
